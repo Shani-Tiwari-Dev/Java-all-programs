73c4471a43768923f5792ef398d7abf6a8b902b4
Shani Tiwari <shanitiwari3354@gmail.com>
2025-07-31 10:10:13 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,10 @@
+class even {
+    public static void main(String[] args)
+    {
+        int num1 = 2;
+        while(num1 <= 10) {
+            System.out.print(num1+"\t");
+            num1 += 2;
+        }
+    }
+}
